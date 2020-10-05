165d54fa64d39fb29cc739afb68da8fe29952bfa
harryxing@hotmail.com <harryxing@hotmail.com>
2020-10-05 11:15:29 -0700

    the 12th commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,54 +1,57 @@
 // Creating map object
 var myMap = L.map("map", {
-    center: [40.7, -73.95],
-    zoom: 11
-  });
-  
-  // Adding tile layer to the map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    center: [60.387172, -153.991983],
+    zoom: 5
+});
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  }).addTo(myMap);
-  
-  // Store API query variables
-  var newtry = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+}).addTo(myMap);
+
+// Store API query variables
+var newtry = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 
-  // Grab the data with d3
-  d3.json(newtry, function(response) {
-  
+// Grab the data with d3
+d3.json(newtry, function (response) {
+
     // Create a new marker cluster group
     var markers = L.markerClusterGroup();
-  
-  
-    response.features.forEach(element => {
-  
-  
-    var location = element.geometry.coordinates;
-  
-      // Check for location property
-      if (location) {
-  
-        var popupString = `<h3>Earthquake near ${element.properties.place}</h3>`;
-        popupString += `<h3>Coordinates: ${element.geometry.coordinates[0]}, ${element.geometry.coordinates[1]}</h3> <hr>`;
-        popupString += `<h3>Magnitude: ${element.properties.mag}</h3>`;
-        popupString += `<h3>Depth: ${element.geometry.coordinates[2]} KM</h3>`;
-        popupString += `<h3>Magnitude: ${new Date(element.properties.time)}</h3>`;
 
-        // Add a new marker to the cluster group and bind a pop-up
-        markers.addLayer(L.marker([location[1],location[0]])
-          .bindPopup(popupString));
-      }
-  
-      
+    // var markers = L.markerClusterGroup({
+    //     maxClusterRadius: 200
+    // });
+
+    response.features.forEach(element => {
+        var location = element.geometry.coordinates;
+
+        // Check for location property
+        if (location) {
+
+            var popupString = `<h3>Earthquake near ${element.properties.place}</h3>`;
+            popupString += `<h3>Coordinates: ${location[0]}, ${location[1]}</h3> <hr>`;
+            popupString += `<h3>Magnitude: ${element.properties.mag}</h3>`;
+            popupString += `<h3>Depth: ${location[2]} KM</h3>`;
+            popupString += `<h3>Magnitude: ${new Date(element.properties.time)}</h3>`;
+
+            // Add a new marker to the cluster group and bind a pop-up
+            markers.addLayer(L.marker([location[1], location[0]])
+                .bindPopup(popupString));
+        }
+
+
     });
-  
-  
+
+
     myMap.addLayer(markers);
-  
-  });
+
+});
+
+
+
   
